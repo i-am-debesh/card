22fedd00161fcd491cd70cd5152d4b39d44fbcd0
Debesh <debesh391@gmail.com>
2024-05-24 16:18:22 +0530

    done

---
+++ script.js
@@ -0,0 +1,29 @@
+const userNameElement = document.querySelector('.username');
+const passwordElement = document.querySelector('.password');
+const submitBtn = document.querySelector('.button');
+const responseArea = document.getElementById('response');
+
+function correctInputs() {
+    let username = (userNameElement.value).toLowerCase();
+    let password = (passwordElement.value);
+    
+    if(username === "panda" && password === "69") {
+        return true;
+    }else {
+        return false;
+    }
+}
+submitBtn.addEventListener('click', ()=>{
+    if(!correctInputs()) {  
+        responseArea.innerText = 'incorrect password!';
+
+        setTimeout(()=>{
+            responseArea.innerText = '';
+    },1500)
+    }else {
+       window.location.href = "index.html";
+    }
+});
+
+
+
---
+++ script2.js
@@ -0,0 +1,6 @@
+const instaLink = document.getElementById('link-name');
+
+instaLink.addEventListener('click',()=>{
+    window.location.href = "https://instagram.com/iamdebesh391";
+});
+
